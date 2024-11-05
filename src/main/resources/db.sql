f57a052f5a9a3dfed52b8cdc82576c6ef3d5afdb
Maxime De Rycke <113899157+mderycke22@users.noreply.github.com>
2024-11-05 10:13:15 +0100

    Update db.sql

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -3,5 +3,6 @@ CREATE TABLE sensor_values (
     id BIGSERIAL PRIMARY KEY,
     sensor_type VARCHAR(32) NOT NULL,
     sensor_value DOUBLE PRECISION NOT NULL,
-    recorded_at TIMESTAMP NOT NULL
+    recorded_at TIMESTAMP NOT NULL,
+    value_unit VARCHAR(8)
 );
