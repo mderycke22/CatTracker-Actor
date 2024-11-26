b74a94d6bfe70f678e8f21ee81fc66c75c22fc21
Maxime De Rycke <113899157+mderycke22@users.noreply.github.com>
2024-11-26 16:10:37 +0100

    Data examples

---
+++ dispenser_schedule.sql
@@ -0,0 +1,11 @@
+INSERT INTO dispenser_schedule (distribution_time, label, kibbles_amount_value)
+VALUES('08:00:00', 'Distribution matinale', 4);
+
+INSERT INTO dispenser_schedule (distribution_time, label, kibbles_amount_value)
+VALUES('12:30:30', 'Distribution de midi', 3);
+
+INSERT INTO dispenser_schedule (distribution_time, label, kibbles_amount_value)
+VALUES('16:15:00', 'Le gouter', 2);
+
+INSERT INTO dispenser_schedule (distribution_time, label, kibbles_amount_value)
+VALUES('20:30:15', 'Repas du soir', 3);
